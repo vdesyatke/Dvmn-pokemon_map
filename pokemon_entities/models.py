b09703c49466fec5294e5de8940b41cc8533871b
vdesyatke <nikita.andronov@gmail.com>
2024-05-18 19:20:33 +0400

    Add description field to model Pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,9 @@ from django.utils.timezone import now
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(verbose_name='Image', upload_to='images', blank=True)
+    # title_en = models.CharField(max_length=200, blank=True)
+    # title_jp = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
